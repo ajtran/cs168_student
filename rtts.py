0444f751b9a2cf7ceb7b5a8123bea2016d5f40ef
aaron_li <aaron_li@berkeley.edu>
2016-11-06 21:33:24 -0800

    rtts.py updated

--- rtts.py
+++ rtts.py
@@ -1,2 +1,20 @@
 def run_ping(hostnames, num_packets, raw_ping_output_filename, aggregated_ping_output_filename):
-  pass
+	pass
+
+def plot_median_rtt_cdf(agg_ping_results_filename, output_cdf_filename):
+
+	"""
+	this function should take in a filename with the json formatted 
+	aggregated ping data and plot a CDF of the median RTTs for each 
+	website that responds to ping
+
+	"""
+	pass
+
+def plot_ping_cdf(raw_ping_results_filename, output_cdf_filename):
+
+	"""
+	this function should take in a filename with the json formatted 
+	raw ping data for a particular hostname, and plot a CDF of the RTTs
+	"""
+	pass
