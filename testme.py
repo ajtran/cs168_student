f9d8aba66000a7ca4ccd29927c29f29dbe361070
aaron_li <aaron_li@berkeley.edu>
2016-11-07 00:23:05 -0800

    the testme.py works and you should just run it in terminal to understand what im doing

---
+++ testme.py
@@ -0,0 +1,60 @@
+import subprocess
+
+host = "www.google.com"
+
+ping = subprocess.Popen(
+    ["ping", "-c", "100", host],
+    stdout = subprocess.PIPE,
+    stderr = subprocess.PIPE
+)
+
+out, error = ping.communicate()
+
+data = out.decode("utf-8") 
+parse = data.split('\n')
+
+print(data)
+
+
+ind=parse.index("--- www.google.com ping statistics ---")
+
+sublist1 = parse[1:ind-1] #times
+sublist1 = [x for x in sublist1 if not x.startswith("Request timeout for")]
+sublist2 = parse[ind:][1].split()
+
+print(sublist1)
+print(sublist2)
+
+
+
+drop_rate = sublist2[len(sublist2) -3]
+drop_rate = float(drop_rate[:len(drop_rate)-1])
+
+print(drop_rate)
+
+times = [float(x.split()[len(x.split())-2][5:]) for x in sublist1]
+times.sort()
+
+print(times)
+
+MAX = times[len(times)-1]
+print(MAX)
+
+
+if len(times) % 2 != 0:
+
+	split = int(len(times)/2)
+	median = times[split]
+else:
+	
+	split = int(len(times)/2)
+	upper_middle = times[split]
+	lower_middle = times[split-1]
+	median = (upper_middle + lower_middle)/2
+
+print(median)
+
+
+
+
+
