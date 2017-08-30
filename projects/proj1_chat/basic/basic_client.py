f169074ed4ed143ff764c51fb67e5887c4727629
Anthony Tran <ajtran@berkeley.edu>
2017-08-29 23:26:07 -0700

    cleaned up project 4 nested in project 3

--- projects/proj1_chat/basic/basic_client.py
+++
@@ -1,9 +0,0 @@
-import sys
-import socket
-
-if __name__ == "__main__":
-  client_socket = socket.socket()
-  # TA's IP: 52.53.187.155 Port: 2013
-  client_socket.connect((sys.argv[1], int(sys.argv[2])))
-  client_socket.send(raw_input())
-  sys.exit(1)
--- projects/proj3_measurement/rtts.py
+++
@@ -1,2 +0,0 @@
-def run_ping(hostnames, num_packets, raw_ping_output_filename, aggregated_ping_output_filename):
-  pass
